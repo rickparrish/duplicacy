ba759efad8990106495ef38f7408e547c6139352
rickparrish <rickparrish@users.noreply.github.com>
2021-07-02 15:55:11 -0400

    Support business and shared folder testing

--- src/duplicacy_oneclient_test.go
+++ src/duplicacy_oneclient_test.go
@@ -16,53 +16,75 @@ import (
 )
 
 func TestOneDriveClient(t *testing.T) {
+	setTestingT(t)
+	SetLoggingLevel(DEBUG)
+	
+	isBusiness := false
+	isShared := true
+	
+	testDir := "test"
+	if isShared {
+		testDir = "sharedtest"
+	}
+	
+	tokenFile := "one-token.json"
+	if isBusiness {
+		tokenFile = "odb-token.json"
+	}
 
-	oneDriveClient, err := NewOneDriveClient("one-token.json", false)
+	oneDriveClient, err := NewOneDriveClient(tokenFile, isBusiness)
 	if err != nil {
 		t.Errorf("Failed to create the OneDrive client: %v", err)
 		return
 	}
 
 	oneDriveClient.TestMode = true
+	
+	oneDriveClient.DetectSharedStorage(testDir)
 
 	existingFiles, err := oneDriveClient.ListEntries("")
 	for _, file := range existingFiles {
 		fmt.Printf("name: %s, isDir: %t\n", file.Name, len(file.Folder) != 0)
 	}
 
-	testID, _, _, err := oneDriveClient.GetFileInfo("test")
+	testID, _, _, err := oneDriveClient.GetFileInfo(testDir)
 	if err != nil {
 		t.Errorf("Failed to list the test directory: %v", err)
 		return
 	}
 	if testID == "" {
-		err = oneDriveClient.CreateDirectory("", "test")
-		if err != nil {
-			t.Errorf("Failed to create the test directory: %v", err)
+		if isShared {
+			t.Errorf("Test directory must exist for shared testing: " + testDir)
 			return
+		} else {
+			err = oneDriveClient.CreateDirectory("", testDir)
+			if err != nil {
+				t.Errorf("Failed to create the test directory: %v", err)
+				return
+			}
 		}
 	}
 
-	test1ID, _, _, err := oneDriveClient.GetFileInfo("test/test1")
+	test1ID, _, _, err := oneDriveClient.GetFileInfo(testDir + "/test1")
 	if err != nil {
 		t.Errorf("Failed to list the test1 directory: %v", err)
 		return
 	}
 	if test1ID == "" {
-		err = oneDriveClient.CreateDirectory("test", "test1")
+		err = oneDriveClient.CreateDirectory(testDir, "test1")
 		if err != nil {
 			t.Errorf("Failed to create the test1 directory: %v", err)
 			return
 		}
 	}
 
-	test2ID, _, _, err := oneDriveClient.GetFileInfo("test/test2")
+	test2ID, _, _, err := oneDriveClient.GetFileInfo(testDir + "/test2")
 	if err != nil {
 		t.Errorf("Failed to list the test2 directory: %v", err)
 		return
 	}
 	if test2ID == "" {
-		err = oneDriveClient.CreateDirectory("test", "test2")
+		err = oneDriveClient.CreateDirectory(testDir, "test2")
 		if err != nil {
 			t.Errorf("Failed to create the test2 directory: %v", err)
 			return
@@ -86,7 +108,7 @@ func TestOneDriveClient(t *testing.T) {
 
 		fmt.Printf("file: %s\n", filename)
 
-		err = oneDriveClient.UploadFile("test/test1/"+filename, content, 100)
+		err = oneDriveClient.UploadFile(testDir + "/test1/"+filename, content, 100)
 		if err != nil {
 			/*if e, ok := err.(ACDError); !ok || e.Status != 409 */ {
 				t.Errorf("Failed to upload the file %s: %v", filename, err)
@@ -95,28 +117,28 @@ func TestOneDriveClient(t *testing.T) {
 		}
 	}
 
-	entries, err := oneDriveClient.ListEntries("test/test1")
+	entries, err := oneDriveClient.ListEntries(testDir + "/test1")
 	if err != nil {
 		t.Errorf("Error list randomly generated files: %v", err)
 		return
 	}
 
 	for _, entry := range entries {
-		err = oneDriveClient.MoveFile("test/test1/"+entry.Name, "test/test2")
+		err = oneDriveClient.MoveFile(testDir + "/test1/"+entry.Name, testDir + "/test2")
 		if err != nil {
 			t.Errorf("Failed to move %s: %v", entry.Name, err)
 			return
 		}
 	}
 
-	entries, err = oneDriveClient.ListEntries("test/test2")
+	entries, err = oneDriveClient.ListEntries(testDir + "/test2")
 	if err != nil {
 		t.Errorf("Error list randomly generated files: %v", err)
 		return
 	}
 
 	for _, entry := range entries {
-		readCloser, _, err := oneDriveClient.DownloadFile("test/test2/" + entry.Name)
+		readCloser, _, err := oneDriveClient.DownloadFile(testDir + "/test2/" + entry.Name)
 		if err != nil {
 			t.Errorf("Error downloading file %s: %v", entry.Name, err)
 			return
@@ -135,7 +157,7 @@ func TestOneDriveClient(t *testing.T) {
 
 	for _, entry := range entries {
 
-		err = oneDriveClient.DeleteFile("test/test2/" + entry.Name)
+		err = oneDriveClient.DeleteFile(testDir + "/test2/" + entry.Name)
 		if err != nil {
 			t.Errorf("Failed to delete the file %s: %v", entry.Name, err)
 			return
